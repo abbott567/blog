9992c53073678a6516bd0bef371d6571549abfbd
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-07-28 21:13:30 +0100

    Updated markup and comments

--- app.js
+++ app.js
@@ -11,18 +11,21 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 
+// Nunjucks config
 nunjucks.configure('views', {
   watch: true,
   noCache: true,
   express: app
 });
 
+// Middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', require('./routes/index'));
 
 // Catch 404 and forward to error handler
@@ -32,10 +35,7 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// Error handlers
-
-// development error handler
-// will print stacktrace
+// Error handler
 app.use((err, req, res, next) => {
   const env = process.env.NODE_ENV;
   const error = env === 'development' ? err : '';
