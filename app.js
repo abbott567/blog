99ace1dba2ea847c9b4a0309f5e398e75863994b
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-07-28 18:18:53 +0100

    Removed users resources

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -28,8 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
