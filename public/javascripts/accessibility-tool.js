3c71dc9e05acc955416955a6d83e8a5c4fc655ad
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-08-03 23:43:37 +0100

    Updated a11y tool

--- public/javascripts/accessibility-tool.js
+++ public/javascripts/accessibility-tool.js
@@ -1,6 +1,5 @@
 $(document).on('click', '.font-size-change', function () {
   var elements = ['body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-
   for (var i = 0; i < elements.length; i++) {
     var $element = $(elements[i]);
     var fontSize = parseInt($element.css('font-size'), 10) || 0;
@@ -11,19 +10,21 @@ $(document).on('click', '.font-size-change', function () {
 });
 
 $(document).on('click', '.colour-change', function () {
-  var css = $(this).attr('data-colour');
+  var css = $(this).attr('data-theme');
   var $customTheme = $('#custom-css');
   var link = '/stylesheets/' + css + '.css ';
-
-  if ($customTheme === 'default') {
+  if (css === 'default') {
     return $('#custom-css').remove();
   }
-
   if ($customTheme.length) {
     return $customTheme.attr('href', link);
   }
-
   $('head').append(
     '<link id="custom-css" href="' + link + '" rel="stylesheet" type="text/css" />'
   );
+});
+
+$(document).on('click', '.font-change', function () {
+  var font = $(this).attr('data-font');
+  $('body').css('font-family', font);
 });
