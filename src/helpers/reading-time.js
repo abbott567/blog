180840f0db4b6c45a864fa061c9a94b927774818
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-08-20 11:58:14 +0100

    Updated readtime

--- src/helpers/reading-time.js
+++ src/helpers/reading-time.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function readingTime(text) {
-  const wordsPerMinute = 200;
-  const noOfWords = text.split(/\s./g).length;
+  const wordsPerMinute = 260;
+  const noOfWords = text.split(/\s/g).length;
   const minutes = noOfWords / wordsPerMinute;
   const readTime = Math.ceil(minutes);
   return `${readTime} minute read`;
