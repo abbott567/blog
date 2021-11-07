b028f1f42b5aa0e31f177876c27a1d90330a90e4
abbott567 <admin@craigabbott.co.uk>
2021-11-07 14:51:11 +0000

    Updated gulp default task

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp')
 
 if (process.env.NODE_ENV === 'production') {
-  gulp.task('default', gulp.series('sass', 'terser', 'generate-assets'))
+  gulp.task('default', gulp.series('sass', 'terser', 'generate-assets', 'nodemon'))
 } else {
   gulp.task('default', gulp.series('sass', 'terser', 'generate-assets', 'nodemon', 'watch'))
 }
