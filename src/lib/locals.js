8103934db16d68dcf94707d0a8c7814fa7bbf77f
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2022-01-03 19:15:45 +0000

    Updated canonical url

--- src/lib/locals.js
+++ src/lib/locals.js
@@ -2,7 +2,7 @@ function locals (req, res, next) {
   const appURL = 'craigabbott.co.uk'
   res.locals.appName = appURL
   res.locals.appURL = appURL
-  res.locals.canonical = 'http://www.' + appURL + req.originalUrl
+  res.locals.canonical = 'https://www.' + appURL + req.originalUrl
   res.locals.environment = process.env.NODE_ENV
   next()
 }
