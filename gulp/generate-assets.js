a42b0894630327146c509c0b522c45e57c36c326
Craig Abbott <admin@craigabbott.co.uk>
2022-09-27 11:39:15 +0100

    Added slide decks for national coding week

--- gulp/generate-assets.js
+++ gulp/generate-assets.js
@@ -2,13 +2,19 @@ const gulp = require('gulp')
 require('./generate-xml-sitemap')
 require('./generate-xml-feed')
 
-gulp.task('move:images', () => {
+gulp.task('copy:images', () => {
   return gulp.src('./src/assets/images/**.*')
     .pipe(gulp.dest('./public/images'))
 })
 
+gulp.task('copy:downloads', () => {
+  return gulp.src('./src/assets/downloads/**/*.*')
+    .pipe(gulp.dest('./public/downloads'))
+})
+
 gulp.task('generate-assets', gulp.parallel([
-  'move:images',
+  'copy:images',
+  'copy:downloads',
   'generate:sitemap',
   'generate:feed'
 ]))
