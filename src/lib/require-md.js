0614675741cf4d5bf344e56331c9442c433b8745
Craig Abbott <admin@craigabbott.co.uk>
2022-10-14 12:23:56 +0100

    Updated function for reading markdown

--- src/lib/require-md.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const fs = require('fs-jetpack')
-
-function requirePostAsString (slug) {
-  return fs.read(`src/views/posts/${slug}/post.md`)
-}
-
-module.exports = requirePostAsString
--- src/views/pages/post/functions.js
+++ src/views/pages/post/functions.js
@@ -3,7 +3,6 @@
 const fs = require('fs-jetpack')
 const posts = require('../../posts/_config.json')
 const readingTime = require('../../../lib/reading-time')
-const requirePostAsString = require('../../../lib/require-md')
 
 const page = 'post'
 const template = `pages/${page}/template.njk`
@@ -11,7 +10,7 @@ const template = `pages/${page}/template.njk`
 function get (req, res, next) {
   const slug = req.params.slug
   const post = findPostBySlug(slug)
-  const text = requirePostAsString(slug)
+  const text = fs.read(`src/views/posts/${slug}/post.md`)
   const readTime = readingTime(text)
   if (post) {
     const content = fs.read(`src/views/posts/${post.slug}/post.md`)
