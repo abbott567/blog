a3b88fe39ddc83d0522fc2003628495dcf0d0d73
Craig Abbott <admin@craigabbott.co.uk>
2022-10-20 18:16:56 +0100

    progressive enhancement for clickable portfolio

---
+++ src/views/assets/js/make-clickable.js
@@ -0,0 +1,23 @@
+function makeImageClickable () {
+  const sections = document.querySelectorAll('#work-overview .section')
+  sections.forEach(function (section) {
+    const img = section.querySelector('img')
+    const links = section.querySelectorAll('a')
+    const lastLink = links[links.length - 1]
+    if (lastLink && lastLink.innerHTML.includes('deep-dive')) {
+      const url = lastLink.getAttribute('href')
+      if (img) {
+        img.classList.add('clickable')
+        img.addEventListener('click', function (e) {
+          location.href = url
+          e.preventDefault()
+        })
+      }
+    }
+  })
+}
+document.addEventListener('DOMContentLoaded', function () {
+  if ('querySelector' in document) {
+    makeImageClickable()
+  }
+})
