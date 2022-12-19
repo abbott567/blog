162f2d0f2917bb7efc2aa5af671db885e0b897e4
Craig Abbott <admin@craigabbott.co.uk>
2022-12-19 10:20:59 +0000

    Updated caching method

--- src/app/config.mjs
+++ src/app/config.mjs
@@ -3,19 +3,17 @@ import express from 'express'
 import routes from './routes/all.mjs'
 import path from 'path'
 import useHttpsInProd from '../app/server/https/use-in-prod.mjs'
+import setCache from './server/cache-control.mjs'
 
 const app = express()
 useHttpsInProd(app)
 
 // Middleware
 app.use(compression())
+app.use(setCache)
 
 // Static files
-if (process.env.NODE_ENV === 'dev') {
-  app.use(express.static(path.resolve('dist')))
-} else {
-  app.use(express.static(path.resolve('dist'), { maxAge: '30m' }))
-}
+app.use(express.static(path.resolve('dist')))
 
 // Routes
 app.use(routes)
