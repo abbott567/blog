091ea11fb2df926b724b1cd2212257130c2caaec
Craig Abbott <admin@craigabbott.co.uk>
2022-12-19 14:02:00 +0000

    Removed redundant object

--- src/app/server/cache-control/functions.mjs
+++ src/app/server/cache-control/functions.mjs
@@ -7,17 +7,13 @@ function cacheControl (app) {
     cacheControl: true,
     maxAge: '1yr'
   }
-  const pages = {
-    cacheControl: true,
-    maxAge: '1h'
-  }
 
   // Only cache in prod
   if (process.env.NODE_ENV === 'production') {
     app.use('/css', express.static(path.resolve('dist/css'), assets))
     app.use('/scripts', express.static(path.resolve('dist/scripts'), assets))
     app.use('/images', express.static(path.resolve('dist/images'), assets))
-    app.use(express.static(path.resolve('dist'), pages))
+    app.use(express.static(path.resolve('dist')))
   } else {
     app.use(express.static(path.resolve('dist')))
   }
