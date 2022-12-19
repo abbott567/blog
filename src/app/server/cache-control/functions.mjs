5d1ccc341c7bf936cd5c0f548882368f4f34f32c
Craig Abbott <admin@craigabbott.co.uk>
2022-12-19 13:31:49 +0000

    reconfigured caching

--- src/app/server/cache-control/functions.mjs
+++ src/app/server/cache-control/functions.mjs
@@ -3,16 +3,16 @@ import path from 'path'
 
 function cacheControl (app) {
   // Config
-  const cacheConfig = {
+  const cacheOpts = {
     cacheControl: true,
     maxAge: '1yr'
   }
 
   // Only cache in prod
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve('dist/css'), cacheConfig))
-    app.use(express.static(path.resolve('dist/images'), cacheConfig))
-    app.use(express.static(path.resolve('dist/scripts'), cacheConfig))
+    app.use('/css', express.static(path.resolve('dist/css'), cacheOpts))
+    app.use('/scripts', express.static(path.resolve('dist/scripts'), cacheOpts))
+    app.use('/images', express.static(path.resolve('dist/images'), cacheOpts))
     app.use(express.static(path.resolve('dist')))
   } else {
     app.use(express.static(path.resolve('dist')))
